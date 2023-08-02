22e649fb3e810209ad59edf198f77b0150b936b4
Jerry_FaGe <619092968@qq.com>
2023-08-03 00:08:49 +0800

    更新信息

--- src/App.vue
+++ src/App.vue
@@ -99,14 +99,16 @@ onMounted(() => {
     "font-size: 20px;font-weight: 600;color: rgb(244,167,89);";
   const styleTitle2 = "font-size:12px;color: rgb(244,167,89);";
   const styleContent = "color: rgb(30,152,255);";
-  const title1 = "無名の主页";
-  const title2 = `
- _____ __  __  _______     ____     __
-|_   _|  \\/  |/ ____\\ \\   / /\\ \\   / /
-  | | | \\  / | (___  \\ \\_/ /  \\ \\_/ /
-  | | | |\\/| |\\___ \\  \\   /    \\   /
- _| |_| |  | |____) |  | |      | |
-|_____|_|  |_|_____/   |_|      |_|`;
+  let title1 = "Jerry_FaGeの主页";
+  let title2 = `
+       _                          ______     _____      
+      | |                        |  ____|   / ____|     
+      | | ___ _ __ _ __ _   _    | |__ __ _| |  __  ___ 
+  _   | |/ _ \\ '__| '__| | | |   |  __/ _/ | | |_ |/ _ \\
+ | |__| |  __/ |  | |  | |_| |   | | | (_| | |__| |  __/
+  \\____/ \\___|_|  |_|   \\__, |   |_|  \\__,_|\\_____|\\___|
+                         __/ |_____                     
+                        |___/______|`;
   const content = `\n\n版本: ${config.version}\n主页: ${config.home}\nGithub: ${config.github}`;
   console.info(
     `%c${title1} %c${title2} %c${content}`,
